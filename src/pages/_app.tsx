a5ee463da1ddc8e91bb038780de6663c9947dfe2
Rodrigo Barraza <hello@rod.dev>
2023-05-19 12:44:48 -0700

    Adding google tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -41,15 +41,31 @@ function App({ Component, pageProps }: AppProps): JSX.Element {
 
     return (
         <Layout>
+            <Script
+                id="google-tag-manager"
+                src="https://www.googletagmanager.com/gtag/js?id=G-R61CVJFDVF"
+                strategy="afterInteractive"
+            />
             <Script 
-            id="google-analytics"
-            strategy="afterInteractive">
+                id="google-tag-manager2"
+                strategy="afterInteractive">
                 {`
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-T62WJS5');
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-R61CVJFDVF');
+                `}
+            </Script>
+            <Script 
+                id="google-analytics"
+                strategy="afterInteractive">
+                {`
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-T62WJS5');
                 `}
             </Script>
             <Component {...pageProps} />
