73dfefb5a8e385cfa60e0c67cb39e35c5fcb81e6
Rodrigo Barraza <hello@rod.dev>
2023-05-22 06:14:57 -0700

    Adding support for more rendering styles

--- src/collections/StyleCollection.ts
+++ src/collections/StyleCollection.ts
@@ -8,7 +8,18 @@ const StyleCollection = [
     { value: 'warcraft', label: 'Fantasy'},
     { value: 'lego', label: 'Toys'},
     { value: 'funko-pop', label: 'Dolls'},
-    { value: 'plastic', label: 'Plastic'},
+    { value: 'plastic', label: 'Plastic Wrap'},
+    { value: 'renaissance', label: 'Renaissance'},
+    { value: 'pixel', label: 'Pixel Art'},
+    { value: 'oil', label: 'Oil Painting'},
+    { value: 'ceramic', label: 'Ceramics'}, // ceramic sculpture statue
+    { value: 'rust', label: 'Rust'}, // corroded rusted metal oxidized
+    { value: 'undead', label: 'Undead'}, // undead zombie
+    { value: 'vampiric', label: 'Vampiric'}, // a vampire dracula, with blood red water on fangs
+    { value: 'vaporwave', label: 'Vaporwave'}, // neo noir, vaporwave
+    { value: 'fire', label: 'Fire'}, // made out of fire
+    { value: 'water', label: 'Water'}, // made out of water, drops, underwater
+    { value: 'floral', label: 'Floral'}, // surrounded in flowers, floral portrait, a close-up photograph
 ];
 
 export default StyleCollection;
