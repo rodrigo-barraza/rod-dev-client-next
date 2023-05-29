e4867df1d9fafc96cd09887e83a04f0238082744
Rodrigo Barraza <hello@rod.dev>
2023-05-29 09:47:47 -0700

    Show sample generate works on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import lodash from 'lodash'
 import styles from './index.module.scss'
 import ArtCollectionsCollection from '@/collections/ArtCollectionsCollection'
 import UtilityLibrary from '@/libraries/UtilityLibrary'
+import RenderApiLibrary from '@/libraries/RenderApiLibrary'
 import { useRouter } from 'next/router'
 
 export const getServerSideProps = async (context: any) => {
@@ -35,7 +36,19 @@ export default function Index(props) {
     const [shuffledArtCollection, setShuffledArtCollection] = useState([])
 
     useEffect(() => {
-        setShuffledArtCollection(lodash.shuffle(ArtCollectionsCollection))
+        async function getRender() {
+            const collection = lodash.shuffle(ArtCollectionsCollection);
+            const getRender1 = await RenderApiLibrary.getRender('IlH3M_bswMz')
+            const getRender2 = await RenderApiLibrary.getRender('4C5IlvWklYQ')
+            const getRender3 = await RenderApiLibrary.getRender('_Nwlq6_7hvx')
+
+            if (getRender1.data && getRender2.data && getRender3.data) {
+                collection.unshift(getRender1.data, getRender2.data, getRender3.data)
+            }
+            
+            setShuffledArtCollection(collection)
+        }
+        getRender()
     }, [])
 
     return (
@@ -63,37 +76,57 @@ export default function Index(props) {
         <div className="gallery">
             {shuffledArtCollection.map((artCollection, artCollectionIndex) => (
                 <div className="image-container" key={artCollectionIndex}>
-                    <Link 
-                    className="image" 
-                    href={`/collections/${artCollection.path}`}
-                    onMouseOver={(event) => UtilityLibrary.playVideoOnMouseOver(event)}
-                    onMouseLeave={(event) => UtilityLibrary.stopVideoOnMouseOver(event)}>
-                        <div className="the-image">
-                            { !artCollection.works[0].videoPath && artCollection.thumbnail && (
-                                <Image 
-                                src={UtilityLibrary.renderAssetPath(artCollection.thumbnail, artCollection.path)}
-                                alt={artCollection.description}
-                                fill={true}/>
-                            )}
-                            { !artCollection.works[0].videoPath && !artCollection.thumbnail && (
-                                <Image 
-                                src={UtilityLibrary.renderAssetPath(artCollection.works[0].imagePath, artCollection.path)}
-                                alt={artCollection.description}
-                                fill={true}/>
-                            )}
-                            { artCollection.works[0].videoPath && (
-                                <video muted loop itemProp="video"
-                                poster={artCollection.poster ? UtilityLibrary.renderAssetPath(artCollection.poster, artCollection.path) : ''}>
-                                    <source src={UtilityLibrary.renderAssetPath(artCollection.works[0].videoPath, artCollection.path)} type="video/mp4"/>
-                                    Your browser does not support the video tag.
-                                </video>
-                            )}
-                            <div className="inside-description">
-                                <div className="name" itemProp="name">{artCollection.title}</div>
-                                <div className="year" itemProp="dateCreated">{artCollection.year}</div>
+                    { artCollection.prompt && (
+                        <Link 
+                        className="image" 
+                        href={`/generate?id=${artCollection.id}`}>
+                            <div className="the-image">
+                                <img 
+                                    src={artCollection.image}
+                                    alt={artCollection.prompt}
+                                    fill={true}>
+                                </img>
+                                <div className="inside-description">
+                                    <div className="name" itemProp="name">Generate</div>
+                                    <div className="year" itemProp="dateCreated">2023</div>
+                                </div>
                             </div>
-                        </div>
-                    </Link>
+                        </Link>
+                    )}
+                    { artCollection.path && (
+                        <Link 
+                        className="image" 
+                        href={`/collections/${artCollection.path}`}
+                        onMouseOver={(event) => UtilityLibrary.playVideoOnMouseOver(event)}
+                        onMouseLeave={(event) => UtilityLibrary.stopVideoOnMouseOver(event)}>
+                            <div className="the-image">
+                                { !artCollection.works[0].videoPath && artCollection.thumbnail && (
+                                    <Image 
+                                    src={UtilityLibrary.renderAssetPath(artCollection.thumbnail, artCollection.path)}
+                                    alt={artCollection.description}
+                                    fill={true}/>
+                                )}
+                                { !artCollection.works[0].videoPath && !artCollection.thumbnail && (
+                                    <Image 
+                                    src={UtilityLibrary.renderAssetPath(artCollection.works[0].imagePath, artCollection.path)}
+                                    alt={artCollection.description}
+                                    fill={true}/>
+                                )}
+                                { artCollection.works[0].videoPath && (
+                                    <video muted loop itemProp="video"
+                                    poster={artCollection.poster ? UtilityLibrary.renderAssetPath(artCollection.poster, artCollection.path) : ''}>
+                                        <source src={UtilityLibrary.renderAssetPath(artCollection.works[0].videoPath, artCollection.path)} type="video/mp4"/>
+                                        Your browser does not support the video tag.
+                                    </video>
+                                )}
+                                <div className="inside-description">
+                                    <div className="name" itemProp="name">{artCollection.title}</div>
+                                    <div className="year" itemProp="dateCreated">{artCollection.year}</div>
+                                </div>
+                            </div>
+                        </Link>
+
+                    )}
                 </div>
             ))}
         </div>
