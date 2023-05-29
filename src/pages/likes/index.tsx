6c0b5e39fdcee0b8f9b82417cc8dbacf6f6e6575
Rodrigo Barraza <hello@rod.dev>
2023-05-29 16:21:09 -0700

    filteredArray fix

--- src/pages/likes/index.tsx
+++ src/pages/likes/index.tsx
@@ -106,7 +106,7 @@ export default function Renders(props) {
       if (sort === 'oldest') {
         return filteredArray.reverse();
       }
-      return [filteredArray[0]];
+      return filteredArray;
     }
   };
 
