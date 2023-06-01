5f6b59dd0fe031ae4e2ede29de8983e942c6a36e
Rodrigo Barraza <hello@rod.dev>
2023-06-01 05:59:50 -0700

    Fix duplicate props

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,8 +34,8 @@ export default function Document() {
             </Script>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/>
-            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&display=swap" rel="stylesheet"/>
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/>
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&display=swap" rel="stylesheet"/>
         </Head>
             <body>
             <Main />
